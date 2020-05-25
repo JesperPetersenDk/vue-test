1f521b5569351cd172b1bf0a44c4b98c64b334b0
Jesper Petersen <info@ordklar.dk>
2020-05-25 16:40:59 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ title }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  el: "home",
+  data: function() {
+    return {
+      title: "Test Vue js!"
+    };
   }
 };
 </script>
