cfe86bb2341717619589b7b2d695d67425ef5f3e
Jesper Petersen <info@ordklar.dk>
2020-05-25 23:24:00 +0200

    Tilføj af test indholdet

---
+++ src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Test">
+    <h2>Test side</h2>
+    <button v-on:click="clickNow(1)">Click me</button>
+    <p v-if="counter > 0">
+      Count: {{ counter > 20 ? "Større end 20" : "mindre end 20" }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  el: "Test",
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    clickNow: function(step) {
+      this.counter += step;
+    }
+  }
+};
+</script>
